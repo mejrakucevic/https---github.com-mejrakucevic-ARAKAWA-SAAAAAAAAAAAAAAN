502c347c85cfe7e10aac8309bc6d695a59712db0
mejrakucevic <mejrakucevic420@gmail.com>
2024-05-06 18:37:05 +0200

    .

---
+++ udemy react/udemy/src/Main.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    //state
+    this.state = {
+      users: [],
+      loading: false,
+    };
+  }
+
+  getUsers() {
+    this.setState({ loading: true });
+    axios("https://api.randomuser.me/?nat=US&results=5").then((response) =>
+      this.setState({
+        users: response.data.results,
+        loading: false,
+      })
+    );
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <div className="users">
+        {!this.state.loading
+          ? this.state.users.map((user) => (
+              <>
+                <div>
+                  <h3>
+                    {user.name.first} {user.name.last}
+                  </h3>
+                  <div>{user.email}</div>
+                  <hr />
+                </div>
+              </>
+            ))
+          : "Loading"}
+      </div>
+    );
+  }
+}
+
+function Main() {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+}
+
+export default Main;
